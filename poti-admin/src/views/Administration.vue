dbcf051f1bb9a947922db920278cb6ed300d139b
brunojamelli <brunojamelli@ufrn.edu.br>
2021-06-14 23:36:00 -0300

    adm list in cards #7

--- poti-admin/src/views/Administration.vue
+++ poti-admin/src/views/Administration.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
     <h2>administradores por aqui</h2>
+    <div class="grid-container">
+      <Card
+        class="grid-item"
+        v-for="item in adms"
+        :key="item.id"
+        style="width: 22rem; margin-bottom: 1em"
+      >
+        <template #title>
+          <div class="item-title">
+            {{ item.name }}
+          </div>
+        </template>
+        <template #content>
+          <p>
+            {{ item.email }}
+          </p>
+        </template>
+      </Card>
+    </div>
   </div>
 </template>
 
@@ -10,10 +29,12 @@
 <script>
 import ApiService from "../utils/ApiService";
 const http = new ApiService("administrator");
+import Card from "primevue/card";
 
 export default {
   name: "Administration",
   components: {
+    Card,
   },
   data: () => ({
     adms: null,
