ae772d526800e37e0ba567597349e42b21089963
brunojamelli <brunojamelli@ufrn.edu.br>
2021-06-18 14:46:47 -0300

    adding vue router to project

--- poti-admin/src/main.js
+++ poti-admin/src/main.js
@@ -11,6 +11,7 @@ import 'primevue/resources/primevue.min.css';
 // Importing the base PrimeIcon styles
 import 'primeicons/primeicons.css';
 import TabMenu from "primevue/tabmenu";
+import store from './store'
 
 Vue.component('TabMenu', TabMenu);
 
@@ -18,5 +19,6 @@ Vue.config.productionTip = false
 Vue.use(PrimeVue);
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
