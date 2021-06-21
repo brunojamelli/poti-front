5c438ef5176d549898ea4bd1f66e50de0a0f4047
brunojamelli <brunojamelli@ufrn.edu.br>
2021-06-21 13:35:08 -0300

    displaying data in card #11

--- poti-admin/src/views/detail/DetailAdvertiser.vue
+++ poti-admin/src/views/detail/DetailAdvertiser.vue
@@ -1,6 +1,28 @@
 <template>
   <div>
-    {{ advertiser }}
+    <!-- {{ advertiser }} -->
+    <div class="teste">
+      <Card>
+        <template #header>
+          <img
+            class="ads-reader"
+            alt="user header"
+            src="../../assets/logo.png"
+          />
+        </template>
+        <template #title>
+          <div>{{ advertiser.title }}</div>
+        </template>
+        <template #subtitle>
+          <p>{{ advertiser.value }} R$ - {{ advertiser.category }}</p>
+        </template>
+        <template #content>
+          {{ advertiser.description }}
+        </template>
+
+        <template #footer> </template>
+      </Card>
+    </div>
   </div>
 </template>
 
@@ -8,8 +30,26 @@
 </style>
 
 <script>
+import Card from "primevue/card";
+
 export default {
   name: "DetailAdvertiser",
   props: ["advertiser"],
+  components: {
+    Card,
+  },
 };
 </script>
+
+<style scoped>
+.ads-reader {
+  width: 10%;
+  height: 10%;
+  margin-top: 10px;
+}
+.teste {
+  margin-top: 3%;
+  margin-left: 15%;
+  margin-right: 15%;
+}
+</style>
