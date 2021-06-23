8487d21bb185e0caedb1502b20de402cf02be9df
brunojamelli <brunojamelli@ufrn.edu.br>
2021-06-22 22:43:56 -0300

    adding admin login view with vuetify ui lib and login logo #5

--- poti-admin/src/views/Login.vue
+++ poti-admin/src/views/Login.vue
@@ -1,8 +1,47 @@
 <template>
-  <div>
-    <p></p>
-    <h1>um belo login</h1>
-  </div>
+  <v-row justify="center" class="text-center" style="margin-top: 100px">
+    <v-col cols="12" sm="10" md="8" lg="5">
+      <v-card ref="form" elevation="2" outlined>
+        <v-card-title>
+          <v-img src="../assets/login.svg"></v-img>
+        </v-card-title>
+
+        <div v-if="!valid">
+          <v-alert color="red" dense outlined>Credenciais inválidas.</v-alert>
+        </div>
+
+        <v-card-text>
+          <v-text-field
+            v-model="form.username"
+            label="Usuário"
+            outlined
+          ></v-text-field>
+          <v-text-field
+            v-model="form.password"
+            label="Senha"
+            outlined
+            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="show1 ? 'text' : 'password'"
+            @click:append="show1 = !show1"
+          ></v-text-field>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-row align="center" justify="center">
+            <v-btn
+              @click="login"
+              rounded
+              dark
+              color="blue"
+              style="width: 200px; margin-bottom: 40px"
+            >
+              Entrar
+            </v-btn>
+          </v-row>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <style scoped>
@@ -12,5 +51,18 @@
 <script>
 export default {
   name: 'Login',
+    data: () => ({
+    show1: false,
+    valid: true,
+    form: { username: "", password: "" },
+  }),
+  methods: {
+    login: function () {
+      this.$store
+        .dispatch("login", this.form)
+        .then(() => this.$router.push("/"))
+        .catch(() => this.valid=false);
+    },
+  },
 }
 </script>
