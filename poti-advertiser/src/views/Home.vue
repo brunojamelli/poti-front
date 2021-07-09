b555925ea6da5b61184f34eb2b6b6e5303771b9e
brunojamelli <brunojamelli@ufrn.edu.br>
2021-07-09 10:47:07 -0300

    adding a call to the API Service for listing an advertiser's ads #27

--- poti-advertiser/src/views/Home.vue
+++ poti-advertiser/src/views/Home.vue
@@ -11,25 +11,34 @@
       <v-btn depressed color="error"> Invalidos </v-btn>
       <v-btn depressed color="warning"> Inativos </v-btn>
     </v-row>
+    <br />
+    <v-row align="center">
+      {{ announcements }}
+    </v-row>
   </div>
 </template>
 
 <script>
+import ApiService from "../utils/ApiService";
+let http = {};
 export default {
   name: "Home",
 
   components: {},
   data: () => ({
-    
+    announcements: [],
+    advId: 4,
   }),
   methods: {
-
+    async clickValids(){
+      
+    }
   },
-  async created(){
-
+  async created() {
+    http = new ApiService(`announcement/advertiser/${this.advId}`);
+    const response = await http.getList();
+    this.announcements = response.data;
   },
-  async mounted(){
-    
-  }
+  async mounted() {},
 };
 </script>
