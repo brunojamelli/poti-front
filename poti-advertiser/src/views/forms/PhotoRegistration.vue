348d87ff5f0ac23c55cf12ad99c6c44fa771d963
brunojamelli <brunojamelli@ufrn.edu.br>
2021-07-13 22:06:25 -0300

    calling the service to save images of an ad in the database #37

--- poti-advertiser/src/views/forms/PhotoRegistration.vue
+++ poti-advertiser/src/views/forms/PhotoRegistration.vue
@@ -1,8 +1,10 @@
 <template>
   <v-card class="mx-auto" max-width="500" style="margin-top: 20px">
     <v-card-text>
+      <!-- :rules="rules" -->
       <v-file-input
-        :rules="rules"
+        multiple
+        v-model="form.files"
         accept="image/png, image/jpeg, image/bmp"
         placeholder="Adicione fotos ao seu anúncio"
         prepend-icon="mdi-camera"
@@ -23,15 +25,32 @@
   </v-card>
 </template>
 <script>
+// import ApiService from "../../utils/ApiService";
+// const http = new ApiService("photo");
+import apiConfig from "../../utils/config";
+// import { getToken } from "../../utils/auth";
+const headers = () => {
+  return {
+    headers: {
+      // Authorization: `${getToken()}`,
+      "Content-Type": "multipart/form-data",
+    },
+  };
+};
+
 export default {
   name: "PhotoRegistration",
   data: () => ({
-    rules: [
-      (value) =>
-        !value ||
-        value.size < 2000000 ||
-        "As fotos do anuncio não pode ter mais de 2MB",
-    ],
+    // rules: [
+    //   (value) =>
+    //     !value ||
+    //     value.size < 2000000 ||
+    //     "As fotos do anuncio não pode ter mais de 2MB",
+    // ],
+    form: {
+      files: [],
+      an_id: 25,
+    },
   }),
   created() {
     this.$store.commit("setTitle", "Cadastro de Fotos");
@@ -39,10 +58,17 @@ export default {
   methods: {
     async submit(evt) {
       evt.preventDefault();
-    //   http.create(this.form);
+      const formData = new FormData();
+      formData.append("an_id", 4);
+      
+      for (const i of Object.keys(this.form.files)) {
+        formData.append("photo", this.form.files[i]);
+      }
+      await apiConfig.post("photo", formData, headers);
       // this.$alert("Espaço Cadastrado.", "Sucesso", "success");
       alert("foto salva com sucesso !!");
-    //   this.$router.push("/cadastro-fotos");
+      window.console.log(this.form.files);
+      //   this.$router.push("/cadastro-fotos");
     },
   },
 };
