a30d707ed241c0bfc083c2db68f2fffc2e0cedd7
brunojamelli <brunojamelli@ufrn.edu.br>
2021-07-19 21:01:55 -0300

    listing the ad photos on the details screen #36

--- poti-advertiser/src/views/AdDetails.vue
+++ poti-advertiser/src/views/AdDetails.vue
@@ -1,6 +1,30 @@
 <template>
   <v-card class="mx-auto" max-width="500" style="margin-top: 20px">
     <h2 class="text-center">Detalhes Anúncio</h2>
+    <v-row>
+      <v-col
+        v-for="photo in photo_list"
+        :key="photo"
+        class="d-flex child-flex"
+        cols="6"
+      >
+        <v-img
+          :src="`http://localhost:3333/photo/${photo.filename}`"
+          :lazy-src="`https://picsum.photos/10/6?image=${5 + 10}`"
+          aspect-ratio="1.5"
+          class="grey lighten-2"
+        >
+          <template v-slot:placeholder>
+            <v-row class="fill-height ma-0" align="center" justify="center">
+              <v-progress-circular
+                indeterminate
+                color="grey lighten-5"
+              ></v-progress-circular>
+            </v-row>
+          </template>
+        </v-img>
+      </v-col>
+    </v-row>
     <br />
     <v-card-text>
       <div>{{ advertiser.createdAt }}</div>
@@ -23,9 +47,27 @@
   </v-card>
 </template>
 <script>
+import ApiService from "../utils/ApiService";
+
 export default {
   name: "AdDetails",
   props: ["advertiser"],
+  data: () => ({
+    photo_list: [],
+    photo_link: "",
+    first_file: "",
+  }),
+  methods: {},
+  async created() {
+    //buscando fotos do anuncio para a listagem nos cards
+    this.photo_link = `photo/filenames/announcement/${this.advertiser.id}`;
+    const http = new ApiService(this.photo_link);
+    let response = await http.getList();
+    this.photo_list = response.data;
+
+    // this.photo_link = this.photo_list[0].filename;
+    window.console.log(this.photo_list[0].filename);
+  },
 };
 </script>
 <style scoped>
