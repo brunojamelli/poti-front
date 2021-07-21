262e90bd36cabeef69727c8e222310a04d619940
brunojamelli <brunojamelli@ufrn.edu.br>
2021-07-21 19:48:10 -0300

    showing advertiser information on your profile #30

--- poti-advertiser/src/views/Profile.vue
+++ poti-advertiser/src/views/Profile.vue
@@ -2,10 +2,20 @@
   <v-card class="mx-auto" max-width="500">
     <v-card-title class="text-h5"> Meu Perfil </v-card-title>
     <v-card-text>
-      <h4>Whatsapp:</h4>
+      <b>{{ whatsLabel }}</b>
+      {{ userData.whatsapp }}
     </v-card-text>
-    <v-card-text>Email: </v-card-text>
-    <v-card-text>endereço: </v-card-text>
+    <v-card-text>
+      <b>{{ emailLabel }}</b>
+      {{ userData.email }}
+    </v-card-text>
+    <v-card-text v-if="userData.address != ''">
+      <b>{{ addressLabel }}</b>
+      {{ userData.address }}
+    </v-card-text>
+    <v-card-text v-else>
+      <b>Endereço não Informado</b>
+    </v-card-text>
 
     <v-card-actions></v-card-actions>
     <!-- <div class="title"></div> -->
@@ -19,15 +29,22 @@ import decode from "jwt-decode";
 let http = {};
 export default {
   name: "Profile",
-
   components: {},
+  data: () => ({
+    userData: {},
+    whatsLabel: "Whatsapp: ",
+    emailLabel: "Email: ",
+    addressLabel: "Endereço: ",
+  }),
   async created() {
     this.$store.commit("setTitle", "Perfil");
     const userToken = decode(this.$store.state.token);
     window.console.log(userToken.user.id);
     http = new ApiService("advertiser");
-    let data = await http.getOne(userToken.user.id);
-    window.console.log(data);
+    let response = await http.getOne(userToken.user.id);
+    let user = response.data[0];
+    this.userData = user;
+    window.console.log(user);
   },
 };
 </script>
