a3842ab47e192fd74e5cd406b65aa0eda4b19062
brunojamelli <brunojamelli@ufrn.edu.br>
2021-08-27 19:42:42 -0300

    removing add details unused code #48

--- poti-admin/src/views/AdDetails.vue
+++ poti-admin/src/views/AdDetails.vue
@@ -1,30 +1,6 @@
 <template>
   <div>
     <div class="detail-box">
-      <!-- <v-row>
-        <v-col
-          v-for="photo in photo_list"
-          :key="photo.id"
-          class="d-flex child-flex"
-          cols="6"
-        >
-          <v-img
-            :src="`http://localhost:3333/photo/${photo.filename}`"
-            :lazy-src="`https://picsum.photos/10/6?image=${5 + 10}`"
-            aspect-ratio="1.5"
-            class="grey lighten-2"
-          >
-            <template v-slot:placeholder>
-              <v-row class="fill-height ma-0" align="center" justify="center">
-                <v-progress-circular
-                  indeterminate
-                  color="grey lighten-5"
-                ></v-progress-circular>
-              </v-row>
-            </template>
-          </v-img>
-        </v-col>
-      </v-row> -->
       <v-card
         v-if="photo_list.length != 0"
         elevation="5"
